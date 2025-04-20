8eef0a8e897fb3a85b1836b95b7871aebbc53208
Virgile de Lacerda <v.delacerda@virgapps.net>
2025-04-20 23:59:10 +0200

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,61 @@
+<div class="container-fluid py-5">
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-lg-5">
+                <img class="img-fluid rounded" src="img/caroline-causse.jpg" alt="">
+            </div>
+            <div class="col-lg-7 mt-4 mt-lg-0">
+                <h2 class="position-relative text-center bg-white text-primary rounded p-3 mt-4 mb-4 d-none d-lg-block" style="width: 350px; margin-left: -205px;">30 ans d'experience</h2>
+                <h1 class="mb-4">CABINET CAROLINE CAUSSÉ</h1>
+                <h6 class="text-uppercase">Avocats</h6>
+                <p>Diplômée de l’Institut du Droit des Affaires, et du Magistère de Fiscalité des Entreprises, Me. Caroline Caussé, a créé un cabinet qui conseille et accompagne ses clients, entreprises et particuliers,  depuis plus de 30 ans. Le cabinet intervient, sur les problématiques civiles et commerciales, dans les domaines de la famille, de l’immobilier, du droit des affaires et de la réparation du préjudice corporel. Elle exerce avec rigueur, efficacité et discrétion tant dans le domaine du conseil  que du contentieux, tout en proposant conciliation et médiation, aux fins de prévenir les litiges et permettre la résolution de conflits.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid py-5">
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-lg-6" style="min-height: 500px;">
+                <div class="position-relative h-100 rounded overflow-hidden">
+                    <img class="position-absolute w-100 h-100" src="img/feature.jpg" style="object-fit: cover;">
+                </div>
+            </div>
+            <div class="col-lg-6 pt-5 pb-lg-5">
+                <div class="feature-text bg-white rounded p-lg-5">
+                    <p>Depuis 30 ans nous accompagnons nos clients, entreprises et particuliers, pour leur offrir un service complet, dans les étapes de leur vie personnelle ou professionnelle, en mettant à leur service, la rigueur et le savoir faire d’une équipe dédiée et de professionnels partenaires de qualité.</p>
+                    <h2 class="mb-4">Nos atouts</h2>
+                    <div class="d-flex mb-4">
+                        <div class="btn-primary btn-md-square px-3" style="border-radius: 50px;">
+                        </div>
+                        <div class="ml-4">
+                            <h5>Compétence</h5>
+                        </div>
+                    </div>
+                    <div class="d-flex mb-4">
+                        <div class="btn-primary btn-md-square px-3" style="border-radius: 50px;">
+                        </div>
+                        <div class="ml-4">
+                            <h5>Réactivité</h5>
+                        </div>
+                    </div>
+                    <div class="d-flex mb-4">
+                        <div class="btn-primary btn-md-square px-3" style="border-radius: 50px;">
+                        </div>
+                        <div class="ml-4">
+                            <h5>Sérieux</h5>
+                        </div>
+                    </div>
+                    <div class="d-flex">
+                        <div class="btn-primary btn-md-square px-3" style="border-radius: 50px;">
+                        </div>
+                        <div class="ml-4">
+                            <h5>Discrétion</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
