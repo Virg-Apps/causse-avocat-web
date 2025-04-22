3f22205f5946b3a034ccd923302e8cdc7abe4cf1
Virgile de Lacerda <v.delacerda@virgapps.net>
2025-04-22 11:43:59 +0200

    détails savoir faire

--- src/lib/components/SavoirFaire.svelte
+++ src/lib/components/SavoirFaire.svelte
@@ -16,18 +16,45 @@
                     </div>
                     <div class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
                         <h5 class="mb-4 px-4">Bail commercial et d’habitation</h5>
+                        <p>Bail commercial et professionnel</p>
+                        <p>Redaction de contrat, cession de contrat de bail</p> 
+                        <p>Renouvellement</p>
+                        <p>Contentieux de la résiliation</p>
+                    </div>
+                    <div class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
+                        <h5 class="mb-4 px-4">Bail d'habitation</h5>
+                        <p>Problématiques de la décence</p>
+                        <p>Contentieux du paiement, de la résiliation et de l’expulsion</p>
+                        <p>Suivi des procédures d’expulsion et d’exécution des jugements</p>
+                        <p>Actions en responsabilité contre l’Etat</p>
                     </div>
                     <div class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
                         <h5 class="mb-4 px-4">Divorce, liquidation, succession</h5>
+                        <p>Divorces contentieux ou par consentement mutuel par acte d’avocat</p>
+                        <p>Liquidation des intérêts patrimoniaux des époux, ou des concubins</p>
+                        <p>Assistance dans les opérations de règlement des successions</p>
+                        <p>Contentieux des successions, contestation de testament, actions en partage, demande de rapport, de recel</p>
+                        <p>Gestion de l’ndivision</p>
+                        <p>Licitations judiciaires et ventes forcées</p>
                     </div>
                     <div class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
-                        <h5 class="mb-4 px-4">Droit du travail</h5>
+                        <h5 class="mb-4 px-4">Litiges en droit du travail</h5>
+                        <p>Etablissement de contrat</p>
+                        <p>Contentieux de la rupture</p>
+                        <p>Transaction et négociation</p>
                     </div>
                     <div class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
                         <h5 class="mb-4 px-4">Sociétés et entreprises en difficultés</h5>
+                        <p>Procédures de mandat ad hoc, de conciliation, de redressement ou de liquidation judiciaire</p>
+                        <p>Assistance aux créanciers</p>
+                        <p>Procédures de sanctions</p>
                     </div>
                     <div class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
                         <h5 class="mb-4 px-4">Marchés publics et privés</h5>
+                        <p>Contentieux de la passation du contrat de marché</p>
+                        <p>Réclamations préalables CCAG, règlement des litiges, amiable ou contentieux</p>
+                        <p>Contentieux de la responsabilité</p>
+                        <p>Contentieux du paiement, DGD et retenues de garantie</p>
                     </div>
                     <div class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
                         <h5 class="mb-4 px-4">Assurances</h5>
@@ -36,6 +63,20 @@
                     </div>
                     <div class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
                         <h5 class="mb-4 px-4">Réparation du préjudice corporel</h5>
+                        <p>Expertises medicales</p>
+                        <p>Demandes d’indemnisation amiables ou contentieuses</p>
+                    </div>
+                    <div class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
+                        <h5 class="mb-4 px-4">Contentieux civil et commercial</h5>
+                        <p>Contentieux devant les Tribunaux judiciaires et de commerce, en première instance et en appel, sur l’ensemble du territoire national</p>
+                        <p>Contentieux des voies d’exécution et des saisies conservatoires</p>
+                        <p>Transaction, conciliation, médiation et modes alternatifs de règlement des litiges</p>
+                    </div>
+                    <div class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
+                        <h5 class="mb-4 px-4">Droit des sociétés</h5>
+                        <p>Création de société, rédaction de statuts, des pactes entre associés</p>
+                        <p>Suivi des sociétés, modifications statutaires, secrétariat juridique</p>
+                        <p>Cession des titres, du fonds de commerce</p>
                     </div>
                 </div>
             </div>
