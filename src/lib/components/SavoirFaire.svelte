0aef3927e8358d9d7ae855101c3e7e2d4cb0fcc5
Virgile de Lacerda <v.delacerda@virgapps.net>
2025-05-01 08:09:05 +0200

    modification des backgrounds

--- src/lib/components/SavoirFaire.svelte
+++ src/lib/components/SavoirFaire.svelte
@@ -8,7 +8,6 @@
                     <details class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
                         <summary>
                             <h5 class="mb-4 px-4">Contentieux de l'immeuble, copropriété, construction, enchères</h5>
-                            <span class="btn-light btn-sm">En savoir plus...</span>
                         </summary>
                         <p>Expertises immobilieres en cas de non-finitions, non conformités, impropriété et tous autres désordres</p>
                         <p>Demande d’indemnisation amiables ou contentieuses</p>
@@ -17,120 +16,90 @@
                         <p>Achat à la barre du Tribunal</p>
                         <p>Activité de mandataire immobilier</p>
                     </details>
-                    <div class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
-                        <details class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
-                            <summary>
-                                <h5 class="mb-4 px-4">Bail commercial et d’habitation</h5>
-                                <span class="btn-light btn-sm">En savoir plus...</span>
-                            </summary>
-                            <p>Bail commercial et professionnel</p>
-                            <p>Redaction de contrat, cession de contrat de bail</p> 
-                            <p>Renouvellement</p>
-                            <p>Contentieux de la résiliation</p>
-                        </details>
-                    </div>
-                    <div class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
-                        <details class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
-                            <summary>
-                                <h5 class="mb-4 px-4">Bail d'habitation</h5>
-                                <span class="btn-light btn-sm">En savoir plus...</span>
-                            </summary>
-                            <p>Problématiques de la décence</p>
-                            <p>Contentieux du paiement, de la résiliation et de l’expulsion</p>
-                            <p>Suivi des procédures d’expulsion et d’exécution des jugements</p>
-                            <p>Actions en responsabilité contre l’Etat</p>
-                        </details>
-                    </div>
-                    <div class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
-                        <details class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
-                            <summary>
-                                <h5 class="mb-4 px-4">Divorce, liquidation, succession</h5>
-                                <span class="btn-light btn-sm">En savoir plus...</span>
-                            </summary>
-                            <p>Divorces contentieux ou par consentement mutuel par acte d’avocat</p>
-                            <p>Liquidation des intérêts patrimoniaux des époux, ou des concubins</p>
-                            <p>Assistance dans les opérations de règlement des successions</p>
-                            <p>Contentieux des successions, contestation de testament, actions en partage, demande de rapport, de recel</p>
-                            <p>Gestion de l’ndivision</p>
-                            <p>Licitations judiciaires et ventes forcées</p>
-                        </details>
-                    </div>
-                    <div class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
-                        <details class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
-                            <summary>
-                                <h5 class="mb-4 px-4">Litiges en droit du travail</h5>
-                                <span class="btn-light btn-sm">En savoir plus...</span>
-                            </summary>
-                            <p>Etablissement de contrat</p>
-                            <p>Contentieux de la rupture</p>
-                            <p>Transaction et négociation</p>
-                        </details>
-                    </div>
-                    <div class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
-                        <details class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
-                            <summary>
-                                <h5 class="mb-4 px-4">Sociétés et entreprises en difficultés</h5>
-                                <span class="btn-light btn-sm">En savoir plus...</span>
-                            </summary>
-                            <p>Procédures de mandat ad hoc, de conciliation, de redressement ou de liquidation judiciaire</p>
-                            <p>Assistance aux créanciers</p>
-                            <p>Procédures de sanctions</p>
-                        </details>
-                    </div>
-                    <div class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
-                        <details class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
-                            <summary>
-                                <h5 class="mb-4 px-4">Marchés publics et privés</h5>
-                                <span class="btn-light btn-sm">En savoir plus...</span>
-                            </summary>
-                            <p>Contentieux de la passation du contrat de marché</p>
-                            <p>Réclamations préalables CCAG, règlement des litiges, amiable ou contentieux</p>
-                            <p>Contentieux de la responsabilité</p>
-                            <p>Contentieux du paiement, DGD et retenues de garantie</p>
-                        </details>
-                    </div>
-                    <div class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
-                        <details class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
-                            <summary>
-                                <h5 class="mb-4 px-4">Assurances</h5>
-                                <span class="btn-light btn-sm">En savoir plus...</span>
-                            </summary>
-                            <p>Gestion de la relation assuré assureur</p>
-                            <p>Courtage</p>
-                        </details>
-                    </div>
-                    <div class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
-                        <details class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
-                            <summary>
-                                <h5 class="mb-4 px-4">Réparation du préjudice corporel</h5>
-                                <span class="btn-light btn-sm">En savoir plus...</span>
-                            </summary>
-                            <p>Expertises medicales</p>
-                            <p>Demandes d’indemnisation amiables ou contentieuses</p>
-                        </details>
-                    </div>
-                    <div class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
-                        <details class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
-                            <summary>
-                                <h5 class="mb-4 px-4">Contentieux civil et commercial</h5>
-                                <span class="btn-light btn-sm">En savoir plus...</span>
-                            </summary>
-                            <p>Contentieux devant les Tribunaux judiciaires et de commerce, en première instance et en appel, sur l’ensemble du territoire national</p>
-                            <p>Contentieux des voies d’exécution et des saisies conservatoires</p>
-                            <p>Transaction, conciliation, médiation et modes alternatifs de règlement des litiges</p>
-                        </details>
-                    </div>
-                    <div class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
-                        <details class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
-                            <summary>
-                                <h5 class="mb-4 px-4">Droit des sociétés</h5>
-                                <span class="btn-light btn-sm">En savoir plus...</span>
-                            </summary>
-                            <p>Création de société, rédaction de statuts, des pactes entre associés</p>
-                            <p>Suivi des sociétés, modifications statutaires, secrétariat juridique</p>
-                            <p>Cession des titres, du fonds de commerce</p>
-                        </details>
-                    </div>
+                    <details class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
+                        <summary>
+                            <h5 class="mb-4 px-4">Bail commercial et d’habitation</h5>
+                        </summary>
+                        <p>Bail commercial et professionnel</p>
+                        <p>Redaction de contrat, cession de contrat de bail</p> 
+                        <p>Renouvellement</p>
+                        <p>Contentieux de la résiliation</p>
+                    </details>
+                    <details class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
+                        <summary>
+                            <h5 class="mb-4 px-4">Bail d'habitation</h5>
+                        </summary>
+                        <p>Problématiques de la décence</p>
+                        <p>Contentieux du paiement, de la résiliation et de l’expulsion</p>
+                        <p>Suivi des procédures d’expulsion et d’exécution des jugements</p>
+                        <p>Actions en responsabilité contre l’Etat</p>
+                    </details>
+                    <details class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
+                        <summary>
+                            <h5 class="mb-4 px-4">Divorce, liquidation, succession</h5>
+                        </summary>
+                        <p>Divorces contentieux ou par consentement mutuel par acte d’avocat</p>
+                        <p>Liquidation des intérêts patrimoniaux des époux, ou des concubins</p>
+                        <p>Assistance dans les opérations de règlement des successions</p>
+                        <p>Contentieux des successions, contestation de testament, actions en partage, demande de rapport, de recel</p>
+                        <p>Gestion de l’ndivision</p>
+                        <p>Licitations judiciaires et ventes forcées</p>
+                    </details>
+                    <details class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
+                        <summary>
+                            <h5 class="mb-4 px-4">Litiges en droit du travail</h5>
+                        </summary>
+                        <p>Etablissement de contrat</p>
+                        <p>Contentieux de la rupture</p>
+                        <p>Transaction et négociation</p>
+                    </details>
+                    <details class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
+                        <summary>
+                            <h5 class="mb-4 px-4">Sociétés et entreprises en difficultés</h5>
+                        </summary>
+                        <p>Procédures de mandat ad hoc, de conciliation, de redressement ou de liquidation judiciaire</p>
+                        <p>Assistance aux créanciers</p>
+                        <p>Procédures de sanctions</p>
+                    </details>
+                    <details class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
+                        <summary>
+                            <h5 class="mb-4 px-4">Marchés publics et privés</h5>
+                        </summary>
+                        <p>Contentieux de la passation du contrat de marché</p>
+                        <p>Réclamations préalables CCAG, règlement des litiges, amiable ou contentieux</p>
+                        <p>Contentieux de la responsabilité</p>
+                        <p>Contentieux du paiement, DGD et retenues de garantie</p>
+                    </details>
+                    <details class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
+                        <summary>
+                            <h5 class="mb-4 px-4">Assurances</h5>
+                        </summary>
+                        <p>Gestion de la relation assuré assureur</p>
+                        <p>Courtage</p>
+                    </details>
+                    <details class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
+                        <summary>
+                            <h5 class="mb-4 px-4">Réparation du préjudice corporel</h5>
+                        </summary>
+                        <p>Expertises medicales</p>
+                        <p>Demandes d’indemnisation amiables ou contentieuses</p>
+                    </details>
+                    <details class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
+                        <summary>
+                            <h5 class="mb-4 px-4">Contentieux civil et commercial</h5>
+                        </summary>
+                        <p>Contentieux devant les Tribunaux judiciaires et de commerce, en première instance et en appel, sur l’ensemble du territoire national</p>
+                        <p>Contentieux des voies d’exécution et des saisies conservatoires</p>
+                        <p>Transaction, conciliation, médiation et modes alternatifs de règlement des litiges</p>
+                    </details>
+                    <details class="item-savoirfaire d-flex flex-column align-items-center text-center bg-white rounded pt-4">
+                        <summary>
+                            <h5 class="mb-4 px-4">Droit des sociétés</h5>
+                        </summary>
+                        <p>Création de société, rédaction de statuts, des pactes entre associés</p>
+                        <p>Suivi des sociétés, modifications statutaires, secrétariat juridique</p>
+                        <p>Cession des titres, du fonds de commerce</p>
+                    </details>
                 </div>
             </div>
         </div>
@@ -138,7 +107,7 @@
 </div>
 
 <style>
-    .item-savoirfaire:open summary span {
-        display: none;
+    .item-savoirfaire summary:hover {
+        background-color: #e1e1e1;
     }
 </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -43,6 +43,7 @@
 				<div class="navbar-nav mr-auto py-0">
 					<a href="/" class="nav-item nav-link" class:active={pathName == '/'}>Accueil</a>
 					<a href="/#savoir-faire" class="nav-item nav-link">Nos savoir-faire</a>
+					<a href="mailto:conctact@causse-avocat.fr" class="nav-item nav-link">Nous contacter</a>
 					<a href="/politique-de-confidentialite" class="nav-item nav-link" class:active={pathName == '/politique-de-confidentialite'}>Politique de confidentilité</a>
 				</div>
 				<a href="#rdv" class="btn btn-primary mr-3 d-none d-lg-block">Je prends un RDV</a>
@@ -53,5 +54,68 @@
 </header>
 
 {@render children()}
+
+<div class="container-fluid bg-secondary text-white pt-5 px-sm-3 px-md-5" style="margin-top: 90px;">
+	<div class="row mt-5">
+		<div class="col-lg-8">
+			<div>
+				<div class="d-flex justify-content-lg-center p-4" style="background: rgba(256, 256, 256, .05);">
+					<i class="fa fa-2x fa-map-marker-alt text-primary"></i>
+					<div class="ml-3">
+						<h5 class="text-white">Nos bureaux</h5><br/>
+						<p class="m-0">18 Cours Pierre-Puget, 13006 Marseille</p><br/>
+						<!--<iframe title="Cabinet Caussé" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2904.1299240865933!2d5.37373407674974!3d43.29060077112157!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12c9c0b9da773cc9%3A0x2e266804ea03bfbc!2s18%20Cr%20Pierre%20Puget%2C%2013006%20Marseille!5e0!3m2!1sfr!2sfr!4v1745493269008!5m2!1sfr!2sfr" width="300" height="225" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>-->
+						<p class="m-0">8 route de la Sablière - Immeuble le Rond-point, 13011 Marseille</p><br/>
+						<!--<iframe title="Cabinet Caussé" src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d2904.1532870913516!2d5.475626996867079!3d43.29011148072604!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1s8%20route%20de%20la%20Sabli%C3%A8re%20-%20Immeuble%20le%20Rond-point%2C%2013011%20Marseille!5e0!3m2!1sfr!2sfr!4v1745493386967!5m2!1sfr!2sfr" width="300" height="225" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>-->
+						<p class="m-0">1 rue Saint-Antoine, 75004 Paris</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-lg-4">
+			<div class="d-flex justify-content-lg-center p-4" style="background: rgba(256, 256, 256, .05);">
+				<i class="fa fa-2x fa-envelope-open text-primary"></i>
+				<div class="ml-3">
+					<h5 class="text-white">Nous contacter</h5><br/>
+					<p class="m-0">contact@causse-avocat.fr</p><br/>
+					<p class="m-0">04 91 52 32 14</p><br/>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="row pt-5">
+		<div class="col-lg-4 col-md-6 mb-5">
+			<a href="/" class="navbar-brand w-100 h-100 m-0 p-0 d-flex align-items-center justify-content-center">
+				<img src="img/logo.png" height="64" alt=""/>
+				<div class="title">
+					<h1 style="color:white">Cabinet Caussé</h1>
+					<span>Avocats au barreau</span>
+				</div>
+			</a>
+		</div>
+		<div class="col-lg-4 col-md-6 mb-5">
+			<a class="btn btn-lg btn-outline-light btn-lg-square mr-2" href="#"><i class="fab fa-linkedin-in"></i></a>
+		</div>
+		<div class="col-lg-4 col-md-6 mb-5">
+			<h4 class="font-weight-semi-bold text-primary mb-4">Plan du site</h4>
+			<div class="d-flex flex-column justify-content-start">
+				<a class="text-white mb-2" href="/"><i class="fa fa-angle-right mr-2"></i>Accueil</a>
+				<a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Nos savoir-faire</a>
+				<a class="text-white mb-2" href="/#rdv"><i class="fa fa-angle-right mr-2"></i>Je demande un rendez-vous</a>
+				<a class="text-white mb-2" href="/#devis"><i class="fa fa-angle-right mr-2"></i>Je demande un devis</a>
+				<a class="text-white mb-2" href="/politique-de-confidentialite"><i class="fa fa-angle-right mr-2"></i>Politique de confidentilité</a>
+			</div>
+		</div>
+	</div>
+	<div class="row p-4 mt-5 mx-0" style="background: rgba(256, 256, 256, .05);">
+		<div class="col-md-6 text-center text-md-left mb-3 mb-md-0">
+			<p class="m-0 text-white">&copy; <a class="font-weight-bold" href="#">Cabinet Caussé</a></p>
+		</div>
+		<div class="col-md-6 text-center text-md-right">
+			<p class="m-0 text-white">Designed by <a class="font-weight-bold" href="https://htmlcodex.com">HTML Codex</a></p>
+		</div>
+	</div>
+</div>
 
 <Toaster/>
